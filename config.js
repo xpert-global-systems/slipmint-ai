4d5caa7af137b2c08fe6a5ee238bd5ccf4b06f3d
LUCKMAN WORLD <199333163+xpertforextradeinc@users.noreply.github.com>
2026-03-24 06:58:45 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+// telegram-bot/config.js
+
+export const TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN";
+export const TELEGRAM_CHAT_ID = "YOUR_TELEGRAM_CHAT_ID";
+export const GETBLOCK_RPC_URL = "https://eth.getblock.io/YOUR_API_KEY";
+
+// Add wallets you want to track
+export const TARGET_WALLETS = [
+  "0xWallet1",
+  "0xWallet2"
+];
+
+// Minimum ETH amount to trigger an alert
+export const MIN_ETH_ALERT = 1;
